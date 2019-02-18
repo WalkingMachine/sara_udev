545a2835e2d71878772bc1402fa9aa50a8a02b3c
philippe <philippe117@live.fr>
2019-02-17 19:28:14 -0500

    Ajoute les launchfiles et le script pour lancer tout les arduinos en même temps

---
+++ src/arduino_launcher.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import roslaunch
+import rospy
+from os import listdir
+#from rospy import is_shutdown, sleep
+
+#launchPath = '/home/philippe/sara_ws/src/sara_udev/launch/launch_arduino.launch'
+package = 'rosserial_python'
+executable = 'serial_node.py'
+devPath = '/dev/SARA/arduinos/'
+launchPortBase = '_port:='+devPath
+nameBase = "name:=arduino_"
+
+while not rospy.is_shutdown():
+
+    rospy.init_node('en_Mapping', anonymous=True)
+    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    roslaunch.configure_logging(uuid)
+
+    try:
+
+        for name in listdir(devPath):
+            print("name:="+str(name))
+
+            node = roslaunch.core.Node(package, executable)
+            node.args = launchPortBase+name
+            launch = roslaunch.scriptapi.ROSLaunch()
+            launch.start()
+
+            process = launch.launch(node)
+            print process.is_alive()
+
+
+    except:
+        print('No arduino connected. Checking every 5 seconds.')
+        rospy.sleep(5)
+        continue
+
+    break
+
+
+print('All arduinos have started.')
+while not rospy.is_shutdown():
+    rospy.sleep(1)
+process.stop()
+#launch.spin()
